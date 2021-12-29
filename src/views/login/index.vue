b2fed226814cece60ccb72e1adc96f569eec63ed
Daiding-Hu <861096526@qq.com>
2021-12-30 00:16:07 +0800

    项目初始化

---
+++ src/views/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    登录页面
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Login'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
